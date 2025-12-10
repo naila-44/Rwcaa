322e05d35c88ad6117d1a0f74a7673f209889f27
Naila Nisar <nailanisar44@gmail.com>
2025-12-10 14:01:52 +0500

    Add Models folder

---
+++ Models/User.ts
@@ -0,0 +1,9 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new Schema({
+  name: { type: String },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+}, { timestamps: true });
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
