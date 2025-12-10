66542adc62aef4966e9338f8886a48e42b557cf8
Naila Nisar <nailanisar44@gmail.com>
2025-12-10 14:04:11 +0500

    Add about page

---
+++ app/about/Page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/layout/Header";
+
+export default function AboutPage() {
+  return (
+    <div className="container mx-auto py-20">
+      <Header title="About Us" />
+      <p className="mt-6 text-lg">
+        This is the about page content.
+      </p>
+    </div>
+  );
+}
