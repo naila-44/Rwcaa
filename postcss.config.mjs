d753627ec4bc8470833ba1cff55b2f1e370ad024
Naila Nisar <nailanisar44@gmail.com>
2025-12-19 19:29:06 +0500

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,9 @@
-const config = {
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
   plugins: {
-    "@tailwindcss/postcss": {},
+    tailwindcss,
+    autoprefixer,
   },
 };
-
-export default config;
