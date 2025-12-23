13c80d36a9143b433566b97ebea60b4f2e402bd2
Naila Nisar <nailanisar44@gmail.com>
2025-12-23 12:33:32 +0500

    Fix models folder case for Vercel

--- Models/User.ts
+++ Models/User.ts
@@ -1,9 +1,9 @@
 import mongoose, { Schema, model, models } from "mongoose";
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
-const User = models.User || model("User", userSchema);
+const User = models.User || model("User", UserSchema);
 export default User;
