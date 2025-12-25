fde47177ada3d12f672a800196c5dcdcc1f57076
Naila Nisar <nailanisar44@gmail.com>
2025-12-25 17:22:40 +0500

    Update User model

--- Models/User.ts
+++ Models/User.ts
@@ -1,9 +1,17 @@
-import mongoose, { Schema, model, models } from "mongoose";
+import { Schema, model, models } from "mongoose";
 
-const UserSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["admin"],
+      default: "admin",
+    },
+  },
+  { timestamps: true }
+);
 
-const User = models.User || model("User", UserSchema);
-export default User;
+export default models.User || model("User", UserSchema);
